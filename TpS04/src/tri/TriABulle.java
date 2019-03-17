66e9eeab27336c78049e8cb6bb94b37d481e6d1a
Alexandre Moro <alexandre.moro@outlook.com>
2019-03-17 18:48:41 +0100

    Fonction triBulle fonctionne (erreur dans la boucle for)

--- TpS04/src/tri/TriABulle.java
+++ TpS04/src/tri/TriABulle.java
@@ -1,10 +1,63 @@
 package tri;
 
-public class TriABulle {
+import java.util.Arrays;
+
+public class TriABulle{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int tab[] = new int[] {43,1,5,13,4,9};
+		
+		Arrays.stream(tab).forEach(System.out::print);
+		
+		System.out.println();
+		
+		triBulle(tab);
+		
+		Arrays.stream(tab).forEach(System.out::print);
+		
+		System.out.println();
+		
+	
+	}
+	
+	
+	
 
+	private static int[] triBulle(int tableau[]) {
+		
+		System.out.println("Entrée dans la fonction triBulle");
+
+		boolean permut;
+		int indiceMax = tableau.length - 1;
+		int swap = 0;
+		int nombreDePermutation = 0;
+		
+		do {
+			
+			permut = false;
+			
+			for ( int i = 1; i <= indiceMax; i++ ) { // j'ai buté une heure sur le non fonctionnement de la boucle car j'avais mis == 
+				
+				if ( tableau[i-1] > tableau[i] ) {
+					
+					swap = tableau[i-1];
+					tableau[i-1] = tableau[i];
+					tableau[i] = swap;
+					permut = true;
+					nombreDePermutation ++;
+					
+				}	
+			}
+			
+			indiceMax--;
+			
+		} while (permut == true);
+		
+		System.out.println("Tableau trié en: " + nombreDePermutation +" opération(s)");
+		
+		return tableau;
+		
 	}
 
 }
