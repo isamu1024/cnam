7c715330b7f29e813cbeb941efb867044ff8977b
Alexandre Moro <alexandre.moro@outlook.com>
2019-03-24 23:33:21 +0100

    afficherGrille ()
    Création de la méthode pour afficher la grille sans les # du fichier
    texte et replacer les . par des 0

--- NFP136/Sudoku/src/methodes/Driver.java
+++ NFP136/Sudoku/src/methodes/Driver.java
@@ -1,39 +1,14 @@
 package methodes;
 
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.Scanner;
 
 public class Driver {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws FileNotFoundException {
 		
-// découpage de la grille en région
-//____________
-//|re1|re2|re3|
-//|re4|re5|re6|
-//|re7|re8|re9|
-//_____________
-//
-			
-		Region region1 = new Region();
-		Region region2 = new Region();
-		Region region3 = new Region();
-		Region region4 = new Region();
-		Region region5 = new Region();
-		Region region6 = new Region();
-		Region region7 = new Region();
-		Region region8 = new Region();
-		Region region9 = new Region();
-
-		ImportFile.lireFichierTest();
-		ImportFile.lireGrilletest();
+		ImportFile.afficherGrille();
 		
-		try {
-			ImportFile.lireGrilleScanner();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
-
 }
--- NFP136/Sudoku/src/methodes/ImportFile.java
+++ NFP136/Sudoku/src/methodes/ImportFile.java
@@ -8,113 +8,49 @@
 
 package methodes;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class ImportFile {
 
 
-	static void lireFichierTest() {
+	static void afficherGrille () {
+	
+	String current;
 
-		try { // try catch pour lever une exception en cas d'absence de fichier ou d'une erreur de lecture
-
-			BufferedReader in = new BufferedReader(new FileReader("test\\test.txt"));
-
-			//http://www-labs.iro.umontreal.ca/~dift6810/fichier1.pdf  __ exemple manipumation de fichiers
-			//https://stackoverflow.com/questions/1480398/java-reading-a-file-from-current-directory   -- lient expliquant que le txt est par défaut dans le project directory
-
-			String str;
-
-			while ((str=in.readLine()) != null ) {
-
-				System.out.printf("%s",str);
-
-			}
-
-			in.close();
-
-		}catch (IOException e) {		
-			e.printStackTrace();
-		}
-
-
-	}
-
-	static void lireGrilletest() {
-
-		// Lit la 
-
-		try { 
-			BufferedReader in = new BufferedReader(new FileReader("test\\grille1.txt"));
-
-			String str;
-
-			while ((str=in.readLine()) != null) {
-
-				// On ignore les ligns commençant par #
-				if (str.contains("#")) {
-					continue;
-				}
-
-				System.out.printf("%s \n", str);
-
-			}
-
-			in.close();
-
-		}catch (IOException e) {		
-			e.printStackTrace();
-		}
-
-
-
-	}
-
-	static int [][] lireGrilleScanner() throws FileNotFoundException {
-
-		//TODO revoir la levée d'exception
-
-
-		//https://stackoverflow.com/questions/18816688/using-bufferedreader-in-java-to-read-a-line-of-ints-before-reading-lines-of-stri
-
-		int[][] grid = new int[9][9]; // initialisation de la grille
-		int i,j = 0; // initialisation des incréments, i balayage ligne j décalage ligne
-		char current;
-		boolean complete = false;
+	try { 
 
 		Scanner sc = new Scanner(new File("test\\grille1.txt"));
 
-			for (i = 0; i <= grid.length; i ++) {
 
-				// https://www.geeksforgeeks.org/gfact-51-java-scanner-nextchar/
+		while (sc.hasNext()) {
 
-				current = sc.next().charAt(0);
+			current = sc.next();
 
-				if (current == '.') {
+			if (current.contains("#")) {
 
-					grid[j][i] = 0;
+				sc.nextLine();
 
-				} else {
+			} else {
 
-					grid[j][i] = Character.getNumericValue(current);
-
-				}
+				current = current.replace('.', '0');  // String est une variable immuable, pour remplacer une valeur il faut passer par la fonction replace (swap de char)
 				
-				j++;
+				System.out.println(current);
 
 			}
 
-			
-			System.out.println();
-			System.out.println(grid[2][2]);
-			
-		return grid;
+		}
+
+		sc.close();
 
 	}
+
+	catch (IOException e) {		
+		e.printStackTrace();
+	}
+	
+	}
+	
 }
 
 
